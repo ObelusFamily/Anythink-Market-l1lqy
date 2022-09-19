31662af50ba5e5b1edc629d9cd4ffc4f4d7d97e1
Alessandra Laderchi <kylanee@gmail.com>
2022-09-19 13:48:50 +0200

    Added search bar in UI.

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -6,10 +6,14 @@ const Banner = () => {
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
-          <span id="get-part">A place to get</span>
+        <form className="form-inline justify-content-center">
+          <span id="get-part">A place to get </span>
+          <div className="form-group">
+            <input id="search-box" type="text" className="form-control" placeholder="What is it that you truly desire?" ></input>
+            <i className="ion-search"></i>
+          </div>
           <span> the cool stuff.</span>
-        </div>
+        </form>
       </div>
     </div>
   );
