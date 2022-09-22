25a12671bae3bcf744427763968e6dab4b9634b2
Alessandra Laderchi <kylanee@gmail.com>
2022-09-22 15:56:14 +0200

    Implemented search filtering.

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,17 +1,27 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <form className="form-inline justify-content-center">
           <span id="get-part">A place to get</span>
-             <div class="input-group col-4">
-                <input id="search-box"  class="form-control py-2 border-right-0 border" type="search" placeholder="What is it that you truly desire?" />
-                <span class="input-group-append">
-                    <div class="input-group-text bg-white"><i class="ion-search"></i></div>
+             <div className="input-group col-4">
+                <input id="search-box"  
+                    className="form-control py-2 border-right-0 border" 
+                    type="search" 
+                    placeholder="What is it that you truly desire?" 
+                    onChange={ (e) => 
+                      props.onItemsFilter(
+                        e.target.value,
+                        (page) => agent.Items.filterBy(e.target.value, page),
+                        agent.Items.filterBy(e.target.value) ) 
+                    } />
+                <span className="input-group-append">
+                    <div className="input-group-text bg-white"><i className="ion-search"></i></div>
                 </span>
             </div>
           <span>the cool stuff.</span>
