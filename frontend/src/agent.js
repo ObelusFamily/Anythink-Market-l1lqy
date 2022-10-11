e992e472fc81f15d6c7395ca644c836f0ba1eaad
Alessandra Laderchi <kylanee@gmail.com>
2022-10-11 22:03:18 +0200

    Changed expression order.

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -4,9 +4,9 @@ import _superagent from "superagent";
 const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL =
-  process.env.NODE_ENV !== "production"
-    ? process.env.REACT_APP_BACKEND_URL
-    : "https://api.anythink.market";
+  process.env.NODE_ENV === "production"
+  ? "https://api.anythink.market"
+  : process.env.REACT_APP_BACKEND_URL;
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
