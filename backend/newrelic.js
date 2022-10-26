1110a406c1ac9382cc991d88091f8ee9024a349d
Alessandra Laderchi <kylanee@gmail.com>
2022-10-26 18:08:20 +0200

    Added env variable.

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '3c4f73963ad4ecc4b22cd94ee7f736e5FFFFNRAL',
+  license_key: `${NEW_RELIC_LICENSE_KEY}`,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
